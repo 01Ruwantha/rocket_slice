ec57a54b8ed9bc728c40845ad20e6ad7ef4b0db5
01Ruwantha <2000ruwantha@gmail.com>
2026-07-22 16:52:37 +0530

    create bottomNavBar & routes

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,10 +1,74 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rocket_slice/core/widgets/app_bottom_nav_bar.dart';
+import 'package:rocket_slice/features/cart/view/cart_screen.dart';
+import 'package:rocket_slice/features/favourite/view/favorites_screen.dart';
 import 'package:rocket_slice/features/home/view/home_screen.dart';
+import 'package:rocket_slice/features/home/view/product_details_screen.dart';
+import 'package:rocket_slice/features/profile/view/profile_screen.dart';
+import 'package:rocket_slice/features/splash/view/splash_screen.dart';
+import 'route_names.dart';
 
 class AppRouter {
+  static final GlobalKey<NavigatorState> _rootNavigatorKey =
+      GlobalKey<NavigatorState>(debugLabel: 'root');
+  static final GlobalKey<NavigatorState> _shellNavigatorKey =
+      GlobalKey<NavigatorState>(debugLabel: 'shell');
+  // static final rootScaffoldKey = GlobalKey<ScaffoldState>();
+
   static final GoRouter router = GoRouter(
+    navigatorKey: _rootNavigatorKey,
+    initialLocation: RouteNames.splashPath,
     routes: [
-      GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+      // Top-level Splash route
+      GoRoute(
+        path: RouteNames.splashPath,
+        name: RouteNames.splash,
+        builder: (context, state) => const SplashScreen(),
+      ),
+
+      // Shell Route for persistent Bottom Navigation Bar tabs
+      ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (context, state, child) {
+          return ScaffoldWithBottomNavBar(child: child);
+        },
+        routes: [
+          GoRoute(
+            path: RouteNames.homePath,
+            name: RouteNames.home,
+            builder: (context, state) => const HomeScreen(),
+          ),
+          GoRoute(
+            path: RouteNames.cartPath,
+            name: RouteNames.cart,
+            builder: (context, state) => const CartScreen(),
+          ),
+          GoRoute(
+            path: RouteNames.favoritesPath,
+            name: RouteNames.favorites,
+            builder: (context, state) => const FavoritesScreen(),
+          ),
+          GoRoute(
+            path: RouteNames.profilePath,
+            name: RouteNames.profile,
+            builder: (context, state) => const ProfileScreen(),
+          ),
+        ],
+      ),
+
+      // Product Details pushed over root navigator
+      GoRoute(
+        parentNavigatorKey: _rootNavigatorKey,
+        path: RouteNames.productDetailsPath,
+        name: RouteNames.productDetails,
+        builder: (context, state) {
+          final id = state.pathParameters['id'] ?? 'p1';
+          return ProductDetailsScreen(pizzaId: id);
+        },
+      ),
     ],
+    errorBuilder: (context, state) =>
+        Scaffold(body: Center(child: Text('Page not found: ${state.error}'))),
   );
 }
--- lib/core/widgets/app_bottom_nav_bar.dart
+++ lib/core/widgets/app_bottom_nav_bar.dart
@@ -1,12 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:rocket_slice/app/router/route_names.dart';
+import 'package:rocket_slice/app/theme/app_theme.dart';
 
 class ScaffoldWithBottomNavBar extends StatelessWidget {
   final Widget child;
 
   const ScaffoldWithBottomNavBar({super.key, required this.child});
+  static final rootScaffoldKey = GlobalKey<ScaffoldState>();
+
+  int _calculateSelectedIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.path;
+    if (location.startsWith(RouteNames.cartPath)) {
+      return 1;
+    }
+    if (location.startsWith(RouteNames.favoritesPath)) {
+      return 2;
+    }
+    if (location.startsWith(RouteNames.profilePath)) {
+      return 3;
+    }
+    if (location.startsWith(RouteNames.homePath)) {
+      return 0;
+    }
+    return 0;
+  }
+
+  void _onItemTapped(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        context.goNamed(RouteNames.home);
+        break;
+      case 1:
+        context.goNamed(RouteNames.cart);
+        break;
+      case 2:
+        context.goNamed(RouteNames.favorites);
+        break;
+      case 3:
+        context.goNamed(RouteNames.profile);
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: child);
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    final selectedIndex = _calculateSelectedIndex(context);
+
+    return Scaffold(
+      body: child,
+      key: rootScaffoldKey,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: AppTheme.primaryColor,
+        unselectedItemColor: isDark ? Colors.white60 : Colors.black54,
+        backgroundColor: isDark ? AppTheme.darkSurface : AppTheme.lightSurface,
+        elevation: 8,
+        onTap: (index) => _onItemTapped(index, context),
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.local_pizza_rounded),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Stack(
+              clipBehavior: Clip.none,
+              children: [const Icon(Icons.shopping_bag_rounded)],
+            ),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Stack(
+              clipBehavior: Clip.none,
+              children: [const Icon(Icons.favorite_rounded)],
+            ),
+            label: 'Favorites',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.person_rounded),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
   }
 }
